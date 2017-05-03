f98da46523766d78108a1498b1299ed0b1eb07aa
iborysiuk <iborysiuk@ap1.io>
2017-05-02 21:48:10 -0400

    commented log line

--- app/src/main/java/io/ap1/jenkinstest/MainActivity.java
+++ app/src/main/java/io/ap1/jenkinstest/MainActivity.java
@@ -1,8 +1,7 @@
 package io.ap1.jenkinstest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Log.i(TAG, "onCreate: ");
+//    Log.i(TAG, "onCreate: ");
   }
 }
